ee7f2b3d71163b115b2de3f6095e6eab6b430e97
lianguoshiren <2545929591@qq.com>
2025-03-15 22:11:35 +0800

    启用禁用员工账号业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -92,4 +92,17 @@ public class EmployeeController {
 
         return Result.success(pageResult);
     }
+
+    /*
+    * 启用禁用员工账号
+    * @param status
+    * @param id
+    * */
+    @PostMapping("/status/{status}")
+    @ApiOperation("启用禁用员工")
+    public Result startOrStop(@PathVariable Integer status, Long id) {
+        log.info("启用禁用员工，参数为：status={}, id={}", status, id);
+        employeeService.startOrStop(status, id);
+        return Result.success();
+    }
 }
